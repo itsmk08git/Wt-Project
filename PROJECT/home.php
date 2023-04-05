e8c8f7f948d535ab66d9cd355fbe6286e7ff1769
itsmk <mohankoirala201521@gmail.com>
2023-04-05 22:12:04 +0545

    fileadded

---
+++ PROJECT/home.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Poppins:wght@100;200;300;400;500;600;800;900&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <a href="http://localhost/project/" class="logo">Travel <span>Guide!</span></a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#destination">Destination</a></li>
+            <li><a href="#book">Book</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="Signin.php">Sign In</a></li>
+        </ul>
+    </header>
+    <!-- Header End-->
+
+    <!-- Home Section -->
+    <section class="home" id="home">
+        <div class="home-text">
+            <h1><strong style="color: lightskyblue"> Plan,</strong> <br><strong>Book &</strong> <br> <big style="color:white=">Travel</big></h1>
+            <p>Plan Your Trip & Visit the different part of <br>Nepal with our guidence.</p>
+            <a href="#Things" class="home-btn">Explore....</a>
+        </div>
+    </section>
+    <!-- End of Home Section -->
+
+    <!-- Container -->
+    <section class="container" id="Things" style="text-align: center;">
+        <div class="text">
+            <h1>Things To Do In Nepal</h1>
+        </div>
+
+        <a href=""><div class="row-item">
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="images/mount1.jpg">
+                </div>
+                <h4>Mountain Tour</h4>
+                <p>Click here for more..</p>
+            </div></a>
+
+            <a href=""><div class="container-box">
+                <div class="container-img">
+                    <img src="images/historical.webp">
+                </div>
+                <h4>Historical Places</h4>
+                <p>Click here for more....</p>
+            </div></a>
+
+            <a href=""><div class="container-box">
+                <div class="container-img">
+                    <img src="images/wildlile.jpg">
+                </div>
+                <h4>Nature & Wildlife</h4>
+                <p>Click here for more..</p>
+            </div></a>
+
+            <a href=""><div class="container-box">
+                <div class="container-img">
+                    <img src="images/tilicho.webp">
+                </div>
+                <h4>Lakes</h4>
+                <p>Click here for more..</p>
+            </div></a>
+
+            <a href=""><div class="container-box">
+                <div class="container-img">
+                    <img src="images/food01 (1).jpg">
+                </div>
+                <h4>Food Hunt</h4>
+                <p>Click here for more..</p>
+            </div></a>
+
+            <a href=""><div class="container-box">
+                <div class="container-img">
+                    <img src="images/stupas.jpg">
+                </div>
+                <h4>Religious Places</h4>
+                <p>Click here for more..</p>
+            </div></a>
+        </div>
+    </section>
+
+
+    <!-- Pakages section-->
+    <hr>
+    <section class="container" id="destination" style="text-align: center;">
+        <div class="text">
+            <h1>Our Top Destination</h1>
+        </div>
+
+        <div class="row-item">
+            <div class="container-box">
+                <div class="container-img">
+                    <a href="home.html"><img src="images/kathmandu.jpeg"></a>
+                </div>
+                <h4>Kathmandu</h4>
+                <p>Rate:</p>
+                <div class="stars">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="images/historical.webp">
+                </div>
+                <h4>Lalitpur</h4>
+                <p>Rate:</p>
+                <div class="stars">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="images/lakes.jpg">
+                </div>
+                <h4>Pokhara</h4>
+                <p>Rate:</p>
+                <div class="stars">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+
+            <div class="container-box">
+                <div class="container-img">
+                    <a href="home.html"><img src="images/bhakt.webp"></a>
+                </div>
+                <h4>Bhaktpur</h4>
+                <p>Rate:</p>
+                <div class="stars">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="images/mustang.jpg">
+                </div>
+                <h4>Mustang</h4>
+                <p>Rate:</p>
+                <div class="stars">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+
+            <div class="container-box">
+                <div class="container-img">
+                    <img src="images/dharan.jpg">
+                </div>
+                <h4>Dharan</h4>
+                <p>Rate:</p>
+                <div class="stars">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Booking -->
+    <section class="booking" id="book">
+        <h1 class="heading-title">Book Your Trip!</h1>
+        <form action="book_form.php" method="post" class="book-form">
+
+            <div class="flex">
+                <div class="inputbox">
+                    <span>Name:</span>
+                    <input type="text" placeholder="Enter Your Name" name="name">
+                </div>
+                <div class="inputbox">
+                    <span>Email:</span>
+                    <input type="mail" placeholder="Enter Your Email" name="email">
+                </div>
+                <div class="inputbox">
+                    <span>Phone No:</span>
+                    <input type="text" placeholder="Enter Your Phone No." name="number">
+                </div>
+                <div class="inputbox">
+                    <span>Address:</span>
+                    <input type="text" placeholder="Enter Your Address" name="address">
+                </div>
+                <div class="inputbox">
+                    <span>Destination:</span>
+                    <input type="text" placeholder="Enter the place you want to Visit" name="location">
+                </div>
+                <div class="inputbox">
+                    <span>Date:</span>
+                    <input type="date" placeholder="Select the date of Departure" name="address">
+                </div>
+                <div class="inputbox">
+                    <span>Budget:</span>
+                    <input type="number" placeholder="Enter the Amount you want to Spend." name="address">
+                </div>
+                <div class="inputbox">
+                    <span>How Many Days Will You  Stay?</span>
+                    <input type="number" name="Days">
+                </div>
+            </div>
+            <a href="book.php"><input type="submit" value="submit" class="btn" name="send"></a>
+        </form>
+    </section>
+
+    <!-- footer -->
+    <section id="contact">
+        <div class="footer">
+            <div class="main">
+                <div class="list">
+                    <h4>Quick links</h4>
+                    <div class="social">
+                        <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Help</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <!-- <p>If you Have Query or <br>if you wanna give us some feedback, <br>Kindly Mail us.</p> -->
+                    <h4>Connect</h4>
+                    <div class="social">
+                        <a href="https://www.facebook.com/" class="fb"><i class='bx bxl-facebook-circle'></i></a>
+                        <a href="https://www.instagram.com/" class="fb"><i class='bx bxl-instagram'></i></a>
+                        <a href="https://twitter.com/?lang=en" class="fb"><i class='bx bxl-twitter'></i></a>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <!-- <p>If you Have Query or <br>if you wanna give us some feedback, <br>Kindly Mail us.</p> -->
+                    <h4>Contact No.</h4>
+                    <div class="social">
+                        <p>9876543201</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- for Js file -->
+    <script type="text/javascript" src="JS/script.js"></script>
+</body>
+
+</html>
